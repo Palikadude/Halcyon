287da2830b601310b98acc1d21c63d8fad0974f9
Palikadude <john.mcgowan@uconn.edu>
2022-06-01 10:26:57 -0400

    Vileplume dialogue

--- Data/Script/ground/metano_grass_home/metano_grass_home_ch_2.lua
+++ Data/Script/ground/metano_grass_home/metano_grass_home_ch_2.lua
@@ -16,6 +16,8 @@ function metano_grass_home_ch_2.SetupGround()
 end
 
 function metano_grass_home_ch_2.Vileplume_Action(chara, activator)
-	GeneralFunctions.StartConversation(chara, "Placeholder.")
+	GeneralFunctions.StartConversation(chara, "I've been hearing that more and more of these mystery dungeons have been popping up as of late.", "Worried")
+	UI:WaitShowDialogue("Strange thing is,[pause=10] nobody seems to know the actual reason why they've been appearing more...")
+	UI:WaitShowDialogue("It seems as though the world is becoming a progressively more dangerous place by the day...[pause=0] These are scary times to live in...")
 	GeneralFunctions.EndConversation(chara)
 end 
